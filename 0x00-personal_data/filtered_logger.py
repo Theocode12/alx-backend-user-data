fc699ac578e91f548cce8dd7d66f2c82ff5842e6
Theocode12 <vchidubem9@gmail.com>
2023-02-02 20:20:21 +0100

    obfuscate a string

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -5,12 +5,8 @@ from typing import List
 import re
 
 
-def filter_datum(
-    fields: List[str], redaction: str, message: List[str], seperator: str
-):
+def filter_datum(fields: List[str], redaction: str, message: List[str], seperator: str):
     """obfuscating function"""
     for field in fields:
-        message = re.sub(
-            r"(?<={}=)[\w\d\/]*".format(field), redaction, message
-        )
+        message = re.sub(r"(?<={}=)[\w\d\/]*".format(field), redaction, message)
     return message
