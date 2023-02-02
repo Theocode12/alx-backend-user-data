c3d7b884a46f0eab7a08a39d661dbd338bc62812
Theocode12 <vchidubem9@gmail.com>
2023-02-02 20:15:20 +0100

    obfuscate a string

---
+++ 0x00-personal_data/filtered_logger.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""A module that returns the log message obfuscated"""
+
+from typing import List
+import re
+
+
+def filter_datum(
+    fields: List[str], redaction: str, message: str, seperator: str
+):
+    """obfuscating function"""
+    for field in fields:
+        message = re.sub(
+            r"(?<={}=)[\w\d\/]*".format(field), redaction, message
+        )
+    return message
