a31b306ba163acd122a7580de7634b9c0dd5c7b0
Theocode12 <vchidubem9@gmail.com>
2023-02-07 18:46:56 +0100

    Basic Auth

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -64,4 +64,4 @@ class BasicAuth(Auth):
     def user_object_from_credentials(
         self, user_email: str, user_pwd: str
     ) -> TypeVar("User"):
-        pass
+        """user object credentials"""
