1090d0ffbc6b2fe6f70297dd68cc298c6d8eb078
Theocode12 <vchidubem9@gmail.com>
2023-02-10 04:31:29 +0100

    session auth

--- 0x02-Session_authentication/api/v1/auth/session_db_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_db_auth.py
@@ -13,7 +13,8 @@ class SessionDbAuth(SessionExpAuth):
     def create_session(self, user_id=None) -> str:
         """create a new session
         Returns:
-            A session id"""
+            A session id
+        """
         session_id = super().create_session(user_id)
         user_session = UserSession(user_id=user_id, session_id=session_id)
         user_session.save()
@@ -34,7 +35,9 @@ class SessionDbAuth(SessionExpAuth):
         return user_id
 
     def destroy_session(self, request=None):
-        """Destroy session based on request cookie"""
+        """
+        Destroy session based on request cookie
+        """
         if super().destroy_session(request):
             session_id = self.session_cookie(request)
             user_session_obj = UserSession.search({"session_id": session_id})
--- 0x02-Session_authentication/models/user_session.py
+++ 0x02-Session_authentication/models/user_session.py
@@ -11,6 +11,7 @@ class UserSession(Base):
     """
 
     def __init__(self, *args: list, **kwargs: dict):
+        """Initializer for UserSession"""
         super().__init__(*args, **kwargs)
         self.user_id = kwargs.get("user_id")
         self.session_id = kwargs.get("session_id")
