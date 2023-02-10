fc0dd61ad5c94b9826807ace25600859972f651b
Theocode12 <vchidubem9@gmail.com>
2023-02-10 04:12:12 +0100

    session auth

--- 0x02-Session_authentication/api/v1/auth/session_db_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_db_auth.py
@@ -3,7 +3,7 @@
 Creation of the SessionDbAuth class
 """
 
-from auth.session_exp_auth import SessionExpAuth
+from api.v1.auth.session_exp_auth import SessionExpAuth
 from models.user_session import UserSession
 
 
@@ -23,6 +23,19 @@ class SessionDbAuth(SessionExpAuth):
         """Returns the User ID by requesting UserSession in the database
         based on session_id
         """
+        user_id = super().user_id_for_session_id(session_id)
+        # if not user_id:
+        #     UserSession.load_from_file()
+        #     user_session_obj = UserSession.search({'session_id': session_id})
+        #     if user_session_obj:
+        #         user_id = user_session_obj[0].user_id
+        #     else:
+        #         user_id = None
+        return user_id
 
     def destroy_session(self, request=None):
         """Destroy session based on request cookie"""
+        if super().destroy_session(request):
+            session_id = self.session_cookie(request)
+            user_session_obj = UserSession.search({"session_id": session_id})
+            user_session_obj[0].remove()
--- 0x02-Session_authentication/models/user_session.py
+++ 0x02-Session_authentication/models/user_session.py
@@ -11,5 +11,6 @@ class UserSession(Base):
     """
 
     def __init__(self, *args: list, **kwargs: dict):
+        super().__init__(*args, **kwargs)
         self.user_id = kwargs.get("user_id")
         self.session_id = kwargs.get("session_id")
