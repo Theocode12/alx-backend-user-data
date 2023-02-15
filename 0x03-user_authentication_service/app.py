6b6316f084525001403c99869818d2e2f041e88a
Theocode12 <vchidubem9@gmail.com>
2023-02-16 00:10:18 +0100

    reset password token endpoint

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -65,5 +65,16 @@ def profile():
     abort(403)
 
 
+@app.route("/reset_password", methods=["POST"])
+def get_reset_password_token():
+    """Sets a reset password token to a user"""
+    try:
+        email = request.form.get("email")
+        token = AUTH.get_reset_password_token(email)
+        return jsonify({"email": f"{email}", "reset_token": f"{token}"})
+    except ValueError:
+        abort(403)
+
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000")
