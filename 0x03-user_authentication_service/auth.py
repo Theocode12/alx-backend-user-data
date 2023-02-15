0521340fe30298579ff31b9e097921513e84bbb5
Theocode12 <vchidubem9@gmail.com>
2023-02-15 19:44:18 +0100

    get user from session

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -56,6 +56,16 @@ class Auth:
         except NoResultFound:
             return None
 
+    def get_user_from_session_id(self, session_id: str) -> User:
+        """Get user from session id"""
+        if session_id:
+            try:
+                user_obj = self._db.find_user_by(session_id=session_id)
+                return user_obj
+            except NoResultFound:
+                pass
+        return None
+
 
 def _generate_uuid() -> str:
     """Generate a unique identifier"""
